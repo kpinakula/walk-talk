93bd793b85ceca6e54c1fad00062b81b3dcbb0ee
Korinna Pinakoulaki <kpinakoulaki@gmail.com>
2020-12-12 03:33:05 +0200

    feat: Bring in some content from contentful for the home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,57 @@
 import React from 'react';
-import { Link } from 'gatsby';
+import { graphql } from 'gatsby';
 
 import Layout from '../components/layout';
 import Container from '../components/container';
 import Title from '../components/title';
 
-export default function Home() {
+import homeStyles from '../styles/home.module.scss';
+
+export default function Home({ data }) {
+  const heroImage = data.allContentfulKeyInfo.edges[0].node.heroImage.fluid.src;
+  const heroImageTitle =
+    data.allContentfulKeyInfo.edges[0].node.heroImage.title;
+  const { tagline } = data.allContentfulKeyInfo.edges[0].node.tagline;
+  const { cta } = data.allContentfulKeyInfo.edges[0].node.cta;
+
   return (
     <Layout>
       <Container>
         <Title titleText="Home" />
-        <Link to="/contact/">Contact</Link>
-        <div>Hello world!</div>
+        <div className={homeStyles.hero}>
+          <img src={heroImage} alt={heroImageTitle}></img>
+          <div>{tagline}</div>
+        </div>
+        <div>{cta}</div>
+        {/* <div>
+          {data.allContentfulWaypoint.edges.map(({ node }, index) => (
+            <p key={index}>{node.name}</p>
+          ))}
+        </div> */}
       </Container>
     </Layout>
   );
 }
+
+export const query = graphql`
+  {
+    allContentfulKeyInfo {
+      edges {
+        node {
+          heroImage {
+            fluid {
+              src
+            }
+            title
+          }
+          tagline {
+            tagline
+          }
+          cta {
+            cta
+          }
+        }
+      }
+    }
+  }
+`;
